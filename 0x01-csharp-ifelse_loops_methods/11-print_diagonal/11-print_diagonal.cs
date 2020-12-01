c828a584b2b7a97f21eb7ec0aec7b47a54409d4e
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-01 12:55:39 -0500

    [Update]First Update

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.Write("\n");
+        }
+        else
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("\\\n");
+                for (int j = 0; j <= i; j++)
+                {
+                    if (i != length - 1)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+            Console.Write("\n");
+        }
+    }
+}
