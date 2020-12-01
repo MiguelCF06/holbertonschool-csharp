2eb6bb5f4b91e88fbdc0117b77728e9537d1c020
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-01 11:29:47 -0500

    [Update]First Update

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i <= 98; i++)
+            {
+                Console.Write($"{i} = 0x{i.ToString("x")}\n");
+            }
+        }
+    }
+}
