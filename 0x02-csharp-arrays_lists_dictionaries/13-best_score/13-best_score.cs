7e810e1867c0e22746a666517dd8b1667989e2cf
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-02 12:07:57 -0500

    [Update]First Update

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
+
+        int maxScore = 0;
+        string student = "";
+        foreach (KeyValuePair<string, int> kvp in myList)
+        {
+            if (kvp.Value > maxScore)
+            {
+                maxScore = kvp.Value;
+                student = kvp.Key;
+            }
+        }
+
+        return student;
+    }
+}
