c90a1c8b14e886175c850922dab3f7a60cd02bd8
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-03 09:22:02 -0500

    [Update]First Update

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> uniqueNums = new HashSet<int>();
+        int sum = 0;
+
+        foreach (int i in myList)
+        {
+            uniqueNums.Add(i);
+        }
+
+        foreach (int i in uniqueNums)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }
+}
