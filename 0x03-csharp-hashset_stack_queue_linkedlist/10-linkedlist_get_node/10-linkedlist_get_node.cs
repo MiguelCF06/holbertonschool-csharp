c47724718665b534bef4dc42b8de4bad4f96c5a3
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-03 11:30:24 -0500

    [Update]First Update

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int counter = 0;
+
+        foreach (int num in myLList)
+        {
+            if (counter == n)
+            {
+                return num;
+            }
+            counter += 1;
+        }
+
+        return 0;
+    }
+}
