04b4a4bbb7c70f94b00fb4173691efd01fa9212b
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-03 11:05:47 -0500

    [Update]First Update

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int counter = 0;
+        foreach (int num in myLList)
+        {
+            counter += 1;
+        }
+
+        return counter;
+    }
+}
