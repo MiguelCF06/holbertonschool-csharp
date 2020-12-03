b40ab372ed6ad8bb928c9a9daff3e15110cd6e97
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-03 10:45:47 -0500

    [Update]First Update

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+
+        if (aStack.Count <= 0)
+        {
+            Console.Write("Stack is empty\n");
+        }
+
+        Console.Write($"Number of items: {aStack.Count}\n");
+        Console.Write($"Top item: {aStack.Peek()}\n");
+        Console.WriteLine($"Stack contains \"{search}\": {aStack.Contains(search)}");
+        if (aStack.Contains(search))
+        {
+            while (aStack.Contains(search))
+            {
+                aStack.Pop();
+            }
+        }
+
+        aStack.Push(newItem);
+        
+
+        return aStack;
+    }
+}
