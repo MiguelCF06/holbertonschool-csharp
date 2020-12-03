36cae233e7d45fed4a2cebeff4ff683545e3b3f6
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-03 10:54:41 -0500

    [Update]First Update

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        if (aQueue.Count <= 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+        Console.WriteLine($"First item: {aQueue.Peek()}");
+        Console.WriteLine($"Queue contains \"{search}\": {aQueue.Contains(search)}");
+
+        while (aQueue.Contains(search))
+        {
+            aQueue.Dequeue();
+        }
+
+        aQueue.Enqueue(newItem);
+
+        return aQueue;
+    }
+}
