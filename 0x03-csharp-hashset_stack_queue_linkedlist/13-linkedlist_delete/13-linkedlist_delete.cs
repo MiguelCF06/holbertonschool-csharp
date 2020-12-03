834f0191c313e47cb5c90e46a6bc66b10e9b7fcb
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-03 12:03:23 -0500

    [Update]First Update

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int counter = 0;
+        int numToDel = 0;
+
+        foreach (int num in myLList)
+        {
+            if (counter == index)
+                numToDel = num;
+            counter += 1;
+        }
+        myLList.Remove(numToDel);
+    }
+}
