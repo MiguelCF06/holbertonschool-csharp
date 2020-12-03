4e63f209a137cf76bb8afa27456484af2075c77d
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-03 11:55:08 -0500

    [Update]First Update

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        var current = myLList.Find(n);
+        foreach (int num in myLList)
+        {
+            if (num >= n)
+            {
+                current = myLList.Find(num);
+                break;
+            }
+        }
+        return myLList.AddBefore(current, n);
+    }
+}
