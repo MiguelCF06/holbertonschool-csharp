ba495d0958397d567431a70792fd3258a7b0b636
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-03 11:02:49 -0500

    [Update]First Update

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> numbers = new LinkedList<int>();
+        if (size < 0)
+        {
+            return numbers;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            numbers.AddLast(i);
+        }
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
+
+        return numbers;
+    }
+}
