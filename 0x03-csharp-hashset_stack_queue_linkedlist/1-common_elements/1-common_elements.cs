1af8e7299992bb7aca3d8631718b5a647e88cd79
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-03 09:32:58 -0500

    [Update]First Update

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> commonEl = new List<int>();
+
+        commonEl = list1.FindAll(elem => list2.Contains(elem));
+
+        commonEl.Sort();
+        return commonEl;
+    }
+}
