aac4faf523814907231d61fc8f53e9957717f94d
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-14 09:24:40 -0500

    [Update]First Update

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int counter = 0;
+        try
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+                counter += 1;
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return counter;
+        }
+        return counter;
+    }
+}
