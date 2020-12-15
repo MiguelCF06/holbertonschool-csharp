004cc7110f819cbd548d54cafb8eb78a5fb2d060
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-15 14:59:01 -0500

    [Update]First Update

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Empty public class that defines a Zombie</summary>
+    public class Zombie
+    {
+        int health;
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+    }
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class and takes as 
+        /// parameter<paramref name="value"/> as the amount of health</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+
+        /// <summary>Returns the amount of health of the Zombie.</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
