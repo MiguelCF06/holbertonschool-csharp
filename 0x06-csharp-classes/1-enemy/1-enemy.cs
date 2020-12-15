efe646d341bdd185a626f42f9c193d6831fc7964
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-15 14:45:50 -0500

    [Update]First Update

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Empty public class that defines a Zombie</summary>
+    public class Zombie
+    {
+        /// <summary>The amount of health of the Zombie</summary>
+        public int health;
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
