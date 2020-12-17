bc6026a94c98cd16bb445a508dbe2787926be2ca
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-17 10:09:54 -0500

    [Update]First Update

---
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Unique_WhenPassingANullString_ReturnsMinusOne()
+        {
+            string s = null;
+            int index = Text.Str.UniqueChar(s);
+            Assert.AreEqual(-1, index);
+        }
+
+        [Test]
+        public void Unique_WhenPassingAStringWithRepeatedChars_ReturnsMinusOne()
+        {
+            string s = "Hhhh";
+            int index = Text.Str.UniqueChar(s);
+            Assert.AreEqual(-1, index);
+        }
+
+        [Test]
+        public void Unique_WhenPassingAStringWithDifferentChars_ReturnIndex()
+        {
+            string s = "Hello my dear friend, how are you";
+            int index = Text.Str.UniqueChar(s);
+            Assert.AreEqual(6, index);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Text
+{
+    public class Str
+    {
+        public static int UniqueChar(string s)
+        {
+            if (s == null)
+                return -1;
+
+            s = s.ToLower();
+            int counterOfChars;
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                counterOfChars = 0;
+                for (int j = 0; j < s.Length - 1; j++)
+                {
+                    if (i != j)
+                    {
+                        if (s[i] == s[j])
+                            counterOfChars += 1;
+                    }
+                }
+                if (counterOfChars == 0)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
