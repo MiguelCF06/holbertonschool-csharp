3ebafe51c0f9d3265ec57f72c7d600852d8d3e42
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-20 17:19:57 -0500

    [Updated]Changed the methods of the properties and methods list

--- 0x08-csharp-inheritance/3-type_get/3-type_get.cs
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Text;
 
 class Obj
 {
@@ -8,16 +9,16 @@ class Obj
     public static void Print(object myObj)
     {
         TypeInfo t = myObj.GetType().GetTypeInfo();
-        IEnumerable<PropertyInfo> pList = t.DeclaredProperties;
-        IEnumerable<MethodInfo> mList = t.DeclaredMethods;
+        IEnumerable<PropertyInfo> pList = t.GetProperties();
+        IEnumerable<MethodInfo> mList = t.GetMethods();
 
-        Console.WriteLine(t.Name + " " + "Properties:");
+        Console.WriteLine(t.Name + ' ' + "Properties:");
         foreach (PropertyInfo p in pList)
         {
             Console.WriteLine(p.Name);
         }
 
-        Console.WriteLine(t.Name + " " + "Methods:");
+        Console.WriteLine(t.Name + ' ' + "Methods:");
         foreach (MemberInfo m in mList)
         {
             Console.WriteLine(m.Name);
