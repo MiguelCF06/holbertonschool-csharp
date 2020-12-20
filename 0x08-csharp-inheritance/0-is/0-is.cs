b5b2d0ae21bd2b95298238c07841048c200bfdb5
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-20 16:09:51 -0500

    [Update]Added documentation

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -2,6 +2,7 @@
 
 class Obj
 {
+    /// <summary>Check if the object <paramref name="obj" /> is of type int, if it is return true otherwise return false.</summary>
     public static bool IsOfTypeInt(object obj)
     {
         if (obj.GetType() == typeof(int))
