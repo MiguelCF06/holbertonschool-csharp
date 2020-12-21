eadbd2e23e1a31103c0b1c174d8653a2cb5c3c9a
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-21 09:18:45 -0500

    [Update]First Update

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Shape
+{
+    /// <summary>Throws an NotImplementedException with the message Area() is not implemented for now.</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
