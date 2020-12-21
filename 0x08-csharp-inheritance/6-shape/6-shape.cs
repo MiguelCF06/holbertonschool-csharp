fb01bbde2ff3b095ac25cf408d3036b3e0a3cb73
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-21 10:16:39 -0500

    [Update]First Update

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Shape
+{
+    /// <summary>Throws an NotImplementedException with the message Area() is not implemented for now.</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>Gets or set the value of width</summary>
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                width = value;
+        }
+    }
+
+    /// <summary>Gets or set the value of height</summary>
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                height = value;
+        }
+    }
+}
