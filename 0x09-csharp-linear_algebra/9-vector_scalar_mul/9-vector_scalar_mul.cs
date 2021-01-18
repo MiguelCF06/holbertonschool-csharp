fc5a20451567887c0436c76982263aaac1119c76
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-01-18 11:58:23 -0500

    [Update]First Update

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] result = new double[vector.Length];
+        if ((vector.Length == 2) || vector.Length == 3)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                result[i] = vector[i] * scalar;
+            }
+            return result;
+        }
+        else
+        {
+            double[] error = {-1};
+            return error;
+        }
+    }
+}
