84b0e03ea08fcf524241101f2b528d47ad934d37
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-01-18 13:52:20 -0500

    [Update]First Update

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -7,15 +7,17 @@ class MatrixMath
     /// <returns>The sum of both matrices</returns>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        int m1Len = matrix1.GetLength(1);
-        int m2Len = matrix2.GetLength(1);
-        double[,] result = new double[m1Len, m1Len];
+        int m1LenI = matrix1.GetLength(0);
+        int m1LenJ = matrix1.GetLength(1);
+        int m2LenI = matrix2.GetLength(0);
+        int m2LenJ = matrix2.GetLength(1);
+        double[,] result = new double[m1LenJ, m1LenJ];
 
-        if ((m1Len == 2 && m2Len == 2) || (m1Len == 3 && m2Len == 3))
+        if ((m1LenI == 2 && m1LenJ == 2 && m2LenI == 2 && m2LenJ == 2) || (m1LenI == 3 && m1LenJ == 3 && m2LenI == 3 && m2LenJ == 3))
         {
-            for (int i = 0; i < m1Len; i++)
+            for (int i = 0; i < m1LenJ; i++)
             {
-                for (int j = 0; j < m1Len; j++)
+                for (int j = 0; j < m1LenJ; j++)
                 {
                     result[i, j] = matrix1[i, j] + matrix2[i, j];
                 }
@@ -24,8 +26,7 @@ class MatrixMath
         }
         else
         {
-            double[,] error = {{-1}};
-            return error;
+            return new double[,] { { -1 } };
         }
 
     }
