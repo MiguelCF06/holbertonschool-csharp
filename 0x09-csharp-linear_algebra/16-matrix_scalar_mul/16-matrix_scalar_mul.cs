604c32a80eb27c8ef1079c477c73c9ce61268641
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-01-18 14:45:38 -0500

    [Update]First Update

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -7,16 +7,17 @@ class MatrixMath
     /// <returns>The resulting matrix</returns>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        if ((matrix.GetLength(0) == 2) || (matrix.GetLength(0) == 3))
+        double[,] result = new double[matrix.GetLength(1), matrix.GetLength(1)];
+        if ((matrix.GetLength(1) == 2) || (matrix.GetLength(1) == 3))
         {
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    matrix[i, j] = matrix[i, j] * scalar;
+                    result[i, j] = matrix[i, j] * scalar;
                 }
             }
-            return matrix;
+            return result;
         }
         else
         {
