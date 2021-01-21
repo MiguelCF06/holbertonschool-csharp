4cdde9f6cff0eb79aa36387df2102fcdb5cd16f1
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-01-21 16:06:41 -0500

    [Update]First Update

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -9,7 +9,7 @@ class MatrixMath
         double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
         double determinant;
 
-        if (matrix.GetLength(0) != 2 && matrix.GetLength(1) != 2)
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return new double[,] {{-1}};
 
         determinant = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
