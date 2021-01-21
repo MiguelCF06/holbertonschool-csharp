d460aeacab54ff06634edacefbbc7743bc25b2ec
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-01-21 13:52:44 -0500

    [Update]First Update

--- 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -7,7 +7,7 @@ class MatrixMath
     /// <returns>The resulting matrix</returns>
     public static double[,] Transpose(double[,] matrix)
     {
-        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        double[,] result = new double[matrix.GetLength(1), matrix.GetLength(0)];
         
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
