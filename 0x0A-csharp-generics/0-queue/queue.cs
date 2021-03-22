5b3b3f99232c8f600311dbab2bdc76252d87695e
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-03-22 12:48:09 -0500

    [Update]First UpdatE

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Queue<T>
+{
+    /// <summary>Check and return the type of the Queue's type</summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
