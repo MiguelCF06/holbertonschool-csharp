9cac9a1a4134ad5ef03ec10b3d855d4ca07d5193
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-03-23 11:36:33 -0500

    [Update]First Update

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+///<summary>Base class</summary>
+public abstract class Base
+{
+    ///<summary>The name of the object</summary>
+    public string name { get; set; }
+
+    ///<summary>Overrides the default ToString function to return what I want</summary>
+    public override string ToString()
+    {
+        string valueToReturn = $"{this.name} is a {this.GetType().ToString()}";
+        return valueToReturn;
+    }
+}
+
+class Door : Base, IInteractive
+{
+    ///<summary>The constructor of the Door where sets its name</summary>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    ///<summary>Happens when the object wants to interact with something in this case is the Door class</summary>
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {this.name}. It's locked.");
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability {get; set;}
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected {get; set;}
+    void Collect();
+}
