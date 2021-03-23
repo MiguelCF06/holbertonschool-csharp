24456a886ea9bc9870b7a000b81fde13ac4c1c28
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-03-23 12:41:54 -0500

    [Updated]Added XML documentation

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -3,8 +3,10 @@
 ///<summary>Base class</summary>
 public abstract class Base
 {
+    ///<summary>The name of the object</summary>
     public string name { get; set; }
 
+    ///<summary>Overrides the default ToString function to return what I want</summary>
     public override string ToString()
     {
         string valueToReturn = $"{this.name} is a {this.GetType().ToString()}";
